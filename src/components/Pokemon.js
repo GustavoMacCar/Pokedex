7cbff1f3e5a7bf0ee74e838f02e55eda56f9506d
IzzyAlex <bebelsbcaldas@gmail.com>
2021-04-13 21:17:59 -0300

    Page buttons created

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,11 @@
 function Pokemon({img_path, number, name, kind})
 {
     return <li>
-           <img src={img_path}></img>
-           <span>{number}</span>
-           <h3>{name}</h3>
-           <span>{kind}</span>         
-           </li>
+        <img src={img_path}></img>
+        <span>{number}</span>
+        <h3>{name}</h3>
+        <span>{kind}</span>         
+    </li>
 
 }
 
