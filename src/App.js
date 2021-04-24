1419b6c4a776e5e62be94898a55b2a232a38e0cc
GustavoMacCar <gmcmacar@gmail.com>
2021-04-23 22:33:00 -0300

    buttons are now responding correctly in the favorites list

--- src/App.js
+++ src/App.js
@@ -282,6 +282,15 @@ function App() {
         const favoritePokemons = response.data.pokemons
         const favoritePokemonsList = Object.values(favoritePokemons)
         //console.log(favoritePokemonsList[0])
+        let i = 0;
+
+       
+      for (i = 0; i < favoritePokemonsList.length; i++)
+        {
+          favoritePokemonsList[i].inFavorites = true            
+        }
+
+
         setFavoritesToRender(
           [...favoritePokemonsList]
           )
@@ -376,21 +385,22 @@ useEffect (() => {
     }
 
     
+    
 
   
 }, [favorite]) 
 
 
 useEffect (() => {
-      
+      /*
   let i = 0;
   
   for (i = 0; i < favoritesToRender.length; i ++)
   {
     favoritesToRender[i].inFavorites = true
-  }
+  } */
 
-  console.log(favoritesToRender)
+  //console.log(favoritesToRender)
 
   return () => {
     setFavoritesToRender([])
